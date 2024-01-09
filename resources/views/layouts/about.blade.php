c6abfabc1f67d917991e03235e36da4fcbd575e2
samuel-isirima <samuel.isirima@yahoo.com>
2024-01-09 02:18:56 +0100

    P

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -190,163 +190,7 @@
     </header>
     <!--========== Header Section End ==============-->
 
-    <!--========== breadcrumb Start ==============-->
-    <section class="breadcrumb-wrapper" data-bg-image="assets/images/banner/breadcrumb.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="breadcrumb-content">
-                        <h1 class="breadcrumb-title text-center">ROAD FREIGHT FORWARDING</h1>
-                        <div class="breadcrumb-link">
-                            <span>
-                                <a href="index.html">
-                                    <span>Home</span>
-                                </a>
-                            </span>
-                            >
-                            <span>
-                                <span> Services</span>
-                            </span>
-                            >
-                            <span>
-                                <span> Road Freight</span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--========== breadcrumb End ==============-->
-
-    <!--========== details Start ==============-->
-    <section class="tj-project-details">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="project-details-wrapper">
-                        <div class="tj-project-widget tj-project-image">
-                            <a href="#">
-                                <img src="assets/images/project/project-7.jpg" alt="Project" /></a>
-
-                        </div>
-                        <div class="tj-project-widget project-content-area">
-                            <h3>
-                                <a class="title-link" href="road-freight.html">
-                                    Road Freight Forwarding</a>
-                            </h3>
-                            <p>
-                                When goods have to be delivered to your customer's door, road freight is still
-                                indispensable. And will remain so in future. In recent years, Ranbax has
-                                expanded the options for European distribution by road freight by adding dozens of
-                                lanes. We make up our own daily schedules for packed product lines. This applies both to
-                                full and part truck loads.
-                            </p>
-                        </div>
-                        <div class="tj-project-widget row align-items-center">
-                            <div class="col-lg-5">
-                                <div class="check-image">
-                                    <img src="assets/images/service/service-10.jpg" alt="Project" />
-                                </div>
-                            </div>
-                            <div class="col-lg-7">
-                                <div class="check-list">
-                                    <h3 class="title">Transport Solutions</h3>
-                                    <p>
-                                        Ranbax is the number one logistics supply chain director with a
-                                        maritime character. In addition to overseas import, export and warehousing
-                                        activities, we also handle the distribution of your goods to and from any
-                                        destination in the World. For more than 20 years, our business locations
-                                        situated at inland road, rail and water terminals have been the foundation of
-                                        our operations. Our Express Logistics Services would not be complete without a
-                                        rapid and efficient transport service that is perfectly in line with these
-                                        terminals and which allow us to deliver goods right to your customer’s door.
-                                    </p>
-                                    <ul class="list-gap">
-                                        <li>
-                                            <i class="fa fa-check"></i> Logistics services
-                                        </li>
-                                        <li><i class="fa fa-check"></i>Door to door delivery</li>
-                                        <li>
-                                            <i class="fa fa-check"></i> Reliable services
-                                        </li>
-                                        <li>
-                                            <i class="fa fa-check"></i>Careful Handling of Valuable Goods
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tj-project-widget row align-items-center">
-                            <div class="col-lg-7">
-                                <div class="check-list">
-                                    <h3 class="title">Quality and Reliability</h3>
-                                    <p>
-                                        Your business needs the right goods in the right place at the right time. In
-                                        other words: delivery reliability. That’s why our efforts are registered in
-                                        KPIs. Thanks to our modern tracking & tracing systems, we measure and evaluate
-                                        these indicators per shipment. Our modern information techniques allow us to
-                                        analyse your shipment precisely. Our logistics engineers use this information
-                                        to:
-                                    </p>
-                                    <ul class="list-gap">
-                                        <li>
-                                            <i class="fa fa-check"></i> check the order intake processing.
-                                        </li>
-                                        <li><i class="fa fa-check"></i> measure and evaluate KPIs continually.</li>
-                                        <li>
-                                            <i class="fa fa-check"></i> optimise your logistics process proactively.
-                                        </li>
-                                    </ul>
-                                   
-                                </div>
-                            </div>
-                            <div class="col-lg-5">
-                                <div class="check-image">
-                                    <img src="assets/images/project/project-9.jpg" alt="Project" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--========== details End ==============-->
-
-    <!--=========== Tracking Section Start =========-->
-    <section id="tracking" class="tj-subscribe-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="subscribe-content-box d-flex align-items-center justify-content-between"
-                        data-bg-image="assets/images/banner/subscribe.png">
-                        <div class="subscribe-content d-flex align-items-center">
-                            <div class="mail-icon">
-                                <img src="assets/icons/email.svg" alt="Icon" />
-                            </div>
-                            <div class="subscribe-title">
-                                <h3 class="title">Track Your Item</h3>
-                            </div>
-                        </div>
-                        <div class="subscribe-form d-flex align-items-center">
-                            <div class="subscribe-input">
-                                <input type="text" id="email" name="emailAddress" placeholder="ENTER TRACKING ID"
-                                    required="" />
-                            </div>
-                            <div class="tj-theme-button">
-                                <button class="tj-submit-btn" type="submit" value="submit">
-                                    Track <i class="flaticon-right-1"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--=========== Tracking Section End =========-->
-
+    @yield('content')
     <!--=========== Footer Section Start =========-->
     <footer class="tj-footer-area">
         <div class="container">
@@ -367,18 +211,18 @@
                             <h5 class="title">Our Services</h5>
                         </div>
                         <div class="widget-menu">
-                            <ul>
+                        <ul>
                                 <li>
-                                    <a href="#"> <i class="flaticon-plus"></i> Air Freight</a>
+                                    <a href="/air-freight"> <i class="flaticon-plus"></i> Air Freight</a>
                                 </li>
                                 <li>
-                                    <a href="#"> <i class="flaticon-plus"></i> Ocean Freight</a>
+                                    <a href="/ocean-freight"> <i class="flaticon-plus"></i> Ocean Freight</a>
                                 </li>
                                 <li>
-                                    <a href="#"> <i class="flaticon-plus"></i> Road Freight</a>
+                                    <a href="/road-freight"> <i class="flaticon-plus"></i> Road Freight</a>
                                 </li>
                                 <li>
-                                    <a href="#"> <i class="flaticon-plus"></i> Warehousing and Storage</a>
+                                    <a href="/ware-housing-and-storage"> <i class="flaticon-plus"></i> Warehousing and Storage</a>
                                 </li>
                             </ul>
                         </div>
